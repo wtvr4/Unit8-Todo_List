e4df8c2d1cdba4e58a465a172c8e6dcd9f9db043
Mohamed Badir <glury6@gmail.com>
2023-07-31 15:51:02 +0400

    added the option to reorder and delete tasks

--- Todo List/ContentView.swift
+++ Todo List/ContentView.swift
@@ -3,37 +3,42 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var todos = [
-        Todo(title: "task 1", subtitle: "do it rn"),
-        Todo(title: "task 2"),
-        Todo(title: "task 3")
-]
+        Todo(title: "read", subtitle: "20 pages"),
+        Todo(title: "water the plants"),
+        Todo(title: "drink water")
+    ]
     
     var body: some View {
         NavigationStack {
             
-                List($todos, id: \.id) { $todo in
-                    NavigationLink{
-                        TodoDetailView(todo: $todo)
-                    } label: {
-                        HStack{
-                            Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
-                                .onTapGesture {
-                                    todo.isCompleted.toggle()
-                                }
-                            VStack(alignment: .leading){
-                                Text(todo.title)
+            List($todos, editActions: [.all]) { $todo in
+                NavigationLink{
+                    TodoDetailView(todo: $todo)
+                } label: {
+                    HStack{
+                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                todo.isCompleted.toggle()
+                            }
+                        VStack(alignment: .leading){
+                            Text(todo.title)
+                                .strikethrough(todo.isCompleted)
+                            if !todo.subtitle.isEmpty {
+                                Text(todo.subtitle)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
                                     .strikethrough(todo.isCompleted)
-                                if !todo.subtitle.isEmpty {
-                                    Text(todo.subtitle)
-                                        .font(.footnote)
-                                        .foregroundColor(.gray)
-                                        .strikethrough(todo.isCompleted)
-                                }
                             }
                         }
                     }
+                }
             }
-                .navigationTitle("Todo")
+            .navigationTitle("Todo")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
         }
     }
 }
