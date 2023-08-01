2903abbccf209e9c8b774b24caa9b72390ef5f7a
Mohamed Badir <glury6@gmail.com>
2023-08-02 01:14:42 +0400

    added file

---
+++ Todo List/Model and ViewModel/Todo+Sample.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Todo {
+    
+    static let sampleTodos = [
+        Todo(title: "sleep", isCompleted: true),
+        Todo(title: "read a book", subtitle: "10 pages"),
+        Todo(title: "drink water")
+    ]
+    
+}
---
+++ Todo List/Model and ViewModel/TodoManager.swift
@@ -0,0 +1,43 @@
+import Foundation
+import SwiftUI
+
+class TodoManager: ObservableObject {
+    @Published var todos: [Todo] = [] {
+        didSet {
+            save()
+        }
+    }
+        
+    init() {
+        load()
+    }
+    
+    func getArchiveURL() -> URL {
+        let plistName = "todos.plist"
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        return documentsDirectory.appendingPathComponent(plistName)
+    }
+    
+    func save() {
+        let archiveURL = getArchiveURL()
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedTodos = try? propertyListEncoder.encode(todos)
+        try? encodedTodos?.write(to: archiveURL, options: .noFileProtection)
+    }
+   
+    func loadSampleData() {
+        todos = Todo.sampleTodos
+    }
+    
+    func load() {
+        let archiveURL = getArchiveURL()
+        print(archiveURL)
+        let propertyListDecoder = PropertyListDecoder()
+                
+        if let retrievedTodoData = try? Data(contentsOf: archiveURL),
+            let todosDecoded = try? propertyListDecoder.decode([Todo].self, from: retrievedTodoData) {
+            todos = todosDecoded
+        }
+    }
+}
